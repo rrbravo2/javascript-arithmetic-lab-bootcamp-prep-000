97dd17e584c10b3b0a458be5bb44e8986f5ace32
Ryan Bravo <rrbravo2@gmail.com>
2019-04-18 18:37:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-var number = 10
-
-function add5() {
-  number += 5 
-}
-
-function dividedBy3() {
-  number /= 3
-}
-
-divideBy3()
-
-console.log(number)
-
-add5()
-
-console.log(number)
-
-number = 10
-
-add5()
-
-console.log(number)
-
-dividedBy3()
-
-console.log(number)
+parseInt('2.2222', 10)
 
 
